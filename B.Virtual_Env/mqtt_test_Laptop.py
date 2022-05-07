2d0f99b535234e78290fb382431702a2fe8838db
benoitLdL <benoit@mx3d.com>
2022-05-07 19:14:46 +0200

    update mqtt host

--- B.Virtual_Env/mqtt_test_Laptop.py
+++ B.Virtual_Env/mqtt_test_Laptop.py
@@ -3,13 +3,21 @@
 import paho.mqtt.client as mqtt
 import time
 
-broker_address="192.168.1.15"
+broker_address      = "192.168.1.15"
+client_id           = "2"
+topic_connection    = "mini/connection" 
+topic_data          = "mini/data"
+
 print("creating new instance")
-client = mqtt.Client("2")       #create new instance
+client = mqtt.Client(client_id)       #create new instance
 print("connecting to broker")
 client.connect(broker_address)  #connect to broker
 
+counter = 0
+
 while True:
-    print("Publishing message to topic","mini/test")
-    client.publish("/mini/test","testing connection")
-    time.sleep(0.5) # wait
+    output = "testing connection"
+    print("Publishing '", output, "' to topic",topic_connection)
+    client.publish(topic_connection,output)
+    counter += 1
+    time.sleep(0.1) # wait
