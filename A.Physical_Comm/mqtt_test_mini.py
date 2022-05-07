f0c5243238c70ffd32babf4070e811587e522d52
Benoit-LdL <benoitlagasse@hotmail.com>
2022-05-07 18:15:01 +0100

    update mqtt mini

--- A.Physical_Comm/mqtt_test_mini.py
+++ A.Physical_Comm/mqtt_test_mini.py
@@ -3,20 +3,28 @@
 import paho.mqtt.client as mqtt
 import time
 
-def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+broker_address      = "localhost"
+client_id           = "1"
+topic_connection    = "mini/connection" 
+topic_data          = "mini/data"
 
-broker_address="localhost"
+
+
+def on_message(client, userdata, message):
+    if message.topic == topic_connection and message.payload.decode("utf-8") == "testing connection":
+        print("message received     " , str(message.payload.decode("utf-8")))
+        print("message topic=       " , message.topic)
+        print("message qos=         " , message.qos)
+        print("message retain flag= " , message.retain)
+        client.publish(topic_connection,"OK")
+
 print("creating new instance")
-client = mqtt.Client("1")       #create new instance
+client = mqtt.Client(client_id)       #create new instance
 client.on_message=on_message    #attach function to callback
 print("connecting to broker")
 client.connect(broker_address)  #connect to broker
 client.loop_start()             #start the loop
-print("Subscribing to topic","mini/test")
-client.subscribe("/mini/test")
+print("Subscribing to topic",topic_connection)
+client.subscribe(topic_connection)
 time.sleep(200) # wait
 client.loop_stop() #stop the loop
