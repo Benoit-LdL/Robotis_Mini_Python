a85777a2014e9492fb92e17c4fbd5c08d9d7ce6a
Benoit-LdL <benoitlagasse@hotmail.com>
2022-05-11 11:36:19 +0100

    updated mini mqtt

--- A.Robotis_Mini/mqtt_test_mini.py
+++ A.Robotis_Mini/mqtt_test_mini.py
@@ -11,20 +11,16 @@ topic_data          = "mini/data"
 
 
 def on_message(client, userdata, message):
-    if message.topic == topic_connection and message.payload.decode("utf-8") == "testing connection":
-        print("message received     " , str(message.payload.decode("utf-8")))
-        print("message topic=       " , message.topic)
-        print("message qos=         " , message.qos)
-        print("message retain flag= " , message.retain)
-        client.publish(topic_connection,"OK")
+    if message.topic == topic_connection and message.payload.decode("utf-8"):
+        print("Topic:", message.topic, "| message: ", str(message.payload.decode("utf-8")))
 
 print("creating new instance")
-client = mqtt.Client(client_id)       #create new instance
-client.on_message=on_message    #attach function to callback
+client = mqtt.Client(client_id)                 #create new instance
+client.on_message=on_message                    #attach function to callback
 print("connecting to broker")
-client.connect(broker_address)  #connect to broker
-client.loop_start()             #start the loop
+client.connect(broker_address)                  #connect to broker
+client.loop_start()                             #start the loop
 print("Subscribing to topic",topic_connection)
 client.subscribe(topic_connection)
-time.sleep(200) # wait
-client.loop_stop() #stop the loop
+time.sleep(200)                                 # wait
+client.loop_stop()                              #stop the loop
