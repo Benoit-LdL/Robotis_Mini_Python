b2c04fdeed0e9049329a9ef163f1788dd12a7ca4
benoitLdL <benoit@mx3d.com>
2022-05-11 12:36:27 +0200

    updated host mqtt

--- B.Host/mqtt_test_Laptop.py
+++ B.Host/mqtt_test_Laptop.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+from numpy import concatenate
 import paho.mqtt.client as mqtt
 import time
 import subprocess
+
+from pandas import concat
 
 broker_address      = "0.0.0.0"
 client_id           = "2"
@@ -11,13 +14,14 @@ topic_data          = "mini/data"
 
 counter = 0
 
-output = subprocess.check_output(['sudo', 'iwgetid'])
+output = str(subprocess.check_output(['sudo', 'iwgetid']))
+
 print("==================================")
-if (str(output).find("Robotis_Mini")):
+if (output.find("Robotis_Mini") != -1):
     print("Connected to Robotis Mini Hotspot!")
     broker_address      = "192.168.50.5"
     
-elif (str(output).find("LdL")):
+elif (output.find("LdL") != -1):
     print("Connected to LdL Wifi!")
     broker_address      = "192.168.1.15"
 else:
@@ -28,11 +32,11 @@ print("==================================")
 if broker_address != "0.0.0.0":
     print("creating new instance")
     client = mqtt.Client(client_id)       #create new instance
-    print("connecting to broker")
+    print("connecting to broker: ",broker_address)
     client.connect(broker_address)  #connect to broker
 
     while True:
-        output = "testing connection"
+        output = "Connection test: " + str(counter)
         print("Publishing '", output, "' to topic",topic_connection)
         client.publish(topic_connection,output)
         counter += 1
